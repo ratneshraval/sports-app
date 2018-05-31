a9020377564a5638ac616041d9f60aa107799076
Ratnesh Raval <rraval@cambriasolutions.com>
2018-05-31 10:52:31 -0700

    Adding basic Header and Footer

---
+++ app/javascript/footer/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import AppFooterBar from './AppFooterBar.jsx'
+
+ReactDOM.render(<AppFooterBar />, document.getElementById('footer-root'))
