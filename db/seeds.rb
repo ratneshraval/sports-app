a26b3e59bfba52d31579a07d9627dcc82bbc7a00
Ratnesh Raval <rraval@cambriasolutions.com>
2018-06-27 00:14:43 -0700

    Better seed data :)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Organization.delete_all
-Sport.delete_all
-Team.delete_all
-User.delete_all
-
 org_names = []
 5.times do
   org_names << {name: FFaker::Company.name}
@@ -20,18 +15,25 @@ p 'Created Organizations'
 
 sports = Sport.create!(
   [
-    {name: 'Baseball', icon_name: 'baseball-ball'},
-    {name: 'Football', icon_name: 'football-ball'},
-    {name: 'Basketball', icon_name: 'basketball-ball'},
-    {name: 'Soccer', icon_name: 'futbol'},
-    {name: 'Hockey', icon_name: 'hockey-puck'},
-    {name: 'Table Tennis', icon_name: 'table-tennis'}
+    # {name: 'Baseball', icon_name: 'baseball-ball'},
+    # {name: 'Football', icon_name: 'football-ball'},
+    # {name: 'Basketball', icon_name: 'basketball-ball'},
+    # {name: 'Soccer', icon_name: 'futbol'},
+    # {name: 'Hockey', icon_name: 'hockey-puck'},
+    # {name: 'Table Tennis', icon_name: 'table-tennis'}
+    {name: 'Speed-Walking', icon_name: 'walking'},
+    {name: 'Bird-Watching', icon_name: 'crow'},
+    {name: 'Shopping', icon_name: 'cart-plus'},
+    {name: 'Gambling', icon_name: 'dice'},
+    {name: 'Frog-Jumping', icon_name: 'frog'},
+    {name: 'Racing', icon_name: 'motorcycle'},
+    {name: 'Spying', icon_name: 'user-secret'}
   ]
 )
 p 'Created Sports'
 
 team_objects = []
-10.times do
+20.times do
   s = sports.sample
   team_objects << {
     name: "#{FFaker::Education.school_name} #{s.name}",
@@ -51,15 +53,15 @@ user_objects = []
     height: (100...200).to_a.sample,
     weight: (100...200).to_a.sample,
     is_public: [true, false].sample,
-    teams: teams.sample([1,2,3].sample),
-    sports: sports.sample([1,2,3].sample)
+    teams: teams.sample([1,2,3,4,5].sample),
+    sports: sports.sample([1,2,3,4,5].sample)
   }
 end
 users = User.create!(user_objects)
 p 'Created Users'
 
 event_objects = []
-20.times do
+100.times do
   event_objects << {
     event_date: FFaker::Time.between(5.months.ago, 2.days.ago).change({min: 0, sec: 0}),
     location: "#{FFaker::Venue.name}, #{FFaker::Address.city}",
